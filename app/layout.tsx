f78e3201316700bf0c0ea971d46093b3e64bf6d6
plin2023 <grontylion@foxmail.com>
2023-12-04 14:06:23 +0800

    添加资源文件，修改clerk默认样式

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { zhCN } from '@clerk/localizations'
 export const metadata: Metadata = {
   title: '码上人生',
   description: '一个高质量程序员论坛',
+  icons: ['/assets/images/site-logo.svg'],
 }
 
 export default function RootLayout({
@@ -15,7 +16,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider localization={zhCN}>
+    <ClerkProvider
+      localization={zhCN}
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
       <html lang='zh-CN'>
         <body>{children}</body>
       </html>
