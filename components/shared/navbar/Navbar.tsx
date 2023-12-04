0b4dbeaa1d1d5aef977bf70ac79b46ace6fbcc40
plin2023 <grontylion@foxmail.com>
2023-12-04 20:55:27 +0800

    头部header制作完成

---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import Theme from '@/components/shared/navbar/Theme'
+import MobileNavbar from '@/components/shared/navbar/MobileNavbar'
+import GlobalSearch from '@/components/shared/search/GlobalSearch'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12'>
+      <Link href='/' className='flex cursor-pointer items-center gap-1'>
+        <Image
+          src='/assets/images/site-logo.svg'
+          width={18}
+          height={18}
+          alt='码上人生Logo'
+        />
+        <p className='h2-bold font-lc text-dark-100 dark:text-light-900 max-sm:hidden'>
+          <span className='text-primary-500'>码</span>上人生
+        </p>
+      </Link>
+      <GlobalSearch />
+      <div className='flex-between gap-5'>
+        <Theme />
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl='/'
+            appearance={{
+              elements: {
+                avatarBox: 'h-10 w-10',
+              },
+              variables: {
+                colorPrimary: '#ff7000',
+              },
+            }}
+          />
+        </SignedIn>
+        <MobileNavbar />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
