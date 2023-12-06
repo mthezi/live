969050f8a1f135e8d1558d6c7ece8341248107d7
plin2023 <grontylion@foxmail.com>
2023-12-06 12:23:24 +0800

    首页 搜索、过滤 完成

---
+++ constants/filters.ts
@@ -0,0 +1,41 @@
+export const AnswerFilters = [
+  { name: '最高赞', value: 'highestUpvotes' },
+  { name: '最低赞', value: 'lowestUpvotes' },
+  { name: '最新', value: 'recent' },
+  { name: '最早', value: 'old' },
+]
+
+export const UserFilters = [
+  { name: '新用户', value: 'new_users' },
+  { name: '老用户', value: 'old_users' },
+  { name: '最佳贡献者', value: 'top_contributors' },
+]
+
+export const QuestionFilters = [
+  { name: '最新提问', value: 'most_recent' },
+  { name: '最早提问', value: 'oldest' },
+  { name: '最多投票', value: 'most_voted' },
+  { name: '最多查看', value: 'most_viewed' },
+  { name: '回答最多', value: 'most_answered' },
+]
+
+export const TagFilters = [
+  { name: '热门', value: 'popular' },
+  { name: '最新', value: 'recent' },
+  { name: '名称', value: 'name' },
+  { name: '最早', value: 'old' },
+]
+
+export const HomePageFilters = [
+  { name: '最新', value: 'newest' },
+  { name: '推荐', value: 'recommended' },
+  { name: '频繁', value: 'frequent' },
+  { name: '未回答', value: 'unanswered' },
+]
+
+export const GlobalSearchFilters = [
+  { name: '问题', value: 'question' },
+  { name: '回答', value: 'answer' },
+  { name: '用户', value: 'user' },
+  { name: '标签', value: 'tag' },
+]
