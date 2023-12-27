e6bc84e6df754cb023a110614e02ec90d4c9467e
grontylion <grontylion@foxmail.com>
2023-12-27 20:36:27 +0800

    所有问题

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -42,7 +42,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
   return (
     <>
       <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
-        <h1 className='h1-bold text-dark100_light900'>所有问题123</h1>
+        <h1 className='h1-bold text-dark100_light900'>所有问题</h1>
         <Link href={'/ask-question'} className='flex justify-end max-sm:w-full'>
           <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900'>
             提问
